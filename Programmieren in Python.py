3657fc37ddd0031f1b9c4dfed9b1ef6fbf5be644
talbor_ <194424255+RaiusTalbor@users.noreply.github.com>
2025-04-01 12:35:16 +0200

    Formatanpassung

--- Programmieren in Python.py
+++ Programmieren in Python.py
@@ -15,14 +15,24 @@ Obergrenze = 0
 Untergrenze = 0
 pfad = ''
 nachkomma = 0
+zweiteübergebenevar = 0
+x = 0
+y = 0
+r = 0
+g = 0
+b = 0
+w = 0
+h = 0
+alterWert = 0
+neuerWert = 0
 
-# Variablen -------------------------------------------------------------------------------
+# Variablen ------------------------------------------------------------------
 var = 3
 int() / float() #auch typecasting
 type()			#gibt Typ zurück
 var2 += 1		#erhöht var um 1
 
-# Zeichenketten ---------------------------------------------------------------------------
+# Zeichenketten --------------------------------------------------------------
 str()
 Zeichenkette[0]			#nulltes Element der Zeichenkette
 Zeichenkette[1:3]		#Elemente von 1 bis 2
@@ -40,7 +50,7 @@ Text='''Am Morgen ging {sie} im {ort} spazieren.'''
 print(text.format(sie='Clara', ort='Wald'))
 #\n New Line
 
-# Listen ----------------------------------------------------------------------------------
+# Listen ---------------------------------------------------------------------
 var = 3 * liste[:3] + ['Buh']
 var=[ausdruck] for i in [range/liste] if [Bedingung]:
 #speichert Liste, zum Beispiel gerade Quadratzahlen
@@ -56,21 +66,21 @@ liste.pop(L)				#gibt L wieder und löscht aus Liste
 liste.sort()				#wird aufsteigend sortiert
 liste.reverse()				#Reihenfolge wird umgekehrt
 
-# Dictionary -----------------------------------------------------------------------------
+# Dictionary ----------------------------------------------------------------
 var = {'schlüssel':'Wert', ...}	#wenn Schlüssel aufgerufen, wert wird wiedergegeben
 var[index]						#Aufruf Dictionary
 var.keys()						#liefert alle Schlüssel
 var.values()					#liefert alle Werte
 var=(1,2)						#unveränderliche Tupel (nur ein Wert: #var=(1,))
 
-# Operatoren ------------------------------------------------------------------------------
+# Operatoren -----------------------------------------------------------------
 +, -, *, /, %, ** oder auch pow(basis, potenz)
 in / not in				#enthalten in
 <, >, <=, >=, ==, !=
 and
 or
 
-# Methoden --------------------------------------------------------------------------------
+# Methoden -------------------------------------------------------------------
 print('Ergebnis: ', var, 'Gut gemacht!')
 input('Wert eintragen:')
 len([Zeichenkette/Liste])
@@ -79,7 +89,7 @@ liste = {'a', 'b', 'c'}			#auch ein Set
 range(zahl, zahl2)				#gibt alle Zahlen bis zahl-1 wieder
 round(zahl, nachkomma)			#rundet
 
-# If --------------------------------------------------------------------------------------
+# If -------------------------------------------------------------------------
 if [Bedingung]:
 	[Funktionsblock]
 elif [zweite zu prüfende Bedingung]:
@@ -87,23 +97,23 @@ elif [zweite zu prüfende Bedingung]:
 else:
 	[Funktionsblock]
 	
-#Schleifen --------------------------------------------------------------------------------
+#Schleifen -------------------------------------------------------------------
 while [Bedingung]:
 	[Funktionsblock]
-#------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
 for i in range(0,3):
 	#i nimmt alle Werte von 0 bis 2 an
 for i in [0,1,2]:
 	#selbe wie oben
 	
-# Funktionen/Unterprogramme ---------------------------------------------------------------
+# Funktionen/Unterprogramme --------------------------------------------------
 def fkt (var, zweiteübergebenevar):
 	global var						#in Fkt. auf globale var zugreifen
 	[Funktionsblock]
 	return rückgabewert				#wenn Wiedergabe erfolgen soll
 var=fkt (50,20,var1, ...)			#var nur nötig, wenn return
 
-# Bibliotheken/Module ---------------------------------------------------------------------
+# Bibliotheken/Module --------------------------------------------------------
 from Bibliothek import *
 from Bibliothek import methode
 
@@ -115,7 +125,7 @@ import Bibliothek as kurzname
 import random
 random.randint(Untergrenze, Obergrenze)
 random.choice()			#wählt zufälliges Element aus
-random.shuffle()			#mischt liste
+random.shuffle()		#mischt liste
 
 # Math
 from math import *
@@ -131,7 +141,7 @@ import time
 var=time.localtime()
 var.tm_year()			#auch month, hour, …
 
-# Speichern/Lesen -------------------------------------------------------------------------
+# Speichern/Lesen ------------------------------------------------------------
 import pickle
 f = open(datei, mode='r' [, encoding='utf-8'])
 #r…Lesen, w…Schreiben, a…neuer Text wird angehangen, rb…Binär lesen, wb…Binär schreiben
@@ -143,65 +153,70 @@ f.flush()				#gespeichert, aber nicht geschlossen (txt)
 pickle.dump()			#für Binärdateien: speichern
 pickle.load()			#für Binärdateien: laden
 
-f=open('Ordner/Renndaten.dat', mode='wb') 	#speichern
+#speichern
+f=open('Ordner/Renndaten.dat', mode='wb')
 pickle.dump(liste, f)
 f.close()
 
-f=open(pfad, 'rb')							#lesen
+#lesen
+f=open(pfad, 'rb')
 var=pickle.load(f)
 f.close()
 
+#Filedialog
 from tkinter import filedialog
 pfad=filedialog.askopenfilename() #gibt nur Pfad zurück
 stream=filedialog.askopenfile()
 stream=filedialog.asksaveasfile()
 
-# Fehler abfangen -------------------------------------------------------------------------
+# Fehler abfangen ------------------------------------------------------------
 try:
 	[Funktionsblock]
 except:
 	[Funktionsblock]
 
-# TK --------------------------------------------------------------------------------------
+# TK -------------------------------------------------------------------------
 
-# Fenster ---------------------------------------------------------------------------------
+# Fenster --------------------------------------------------------------------
 from tkinter import *
 fenster=Tk()
 fenster.title('')
 fenster.geometry("1000x800")
 fenster.mainloop()
 
-# Label -----------------------------------------------------------------------------------
+# Label ----------------------------------------------------------------------
 var=Label(master=fenster, text='')
 #rechteckige Fläche
 
-#Button -----------------------------------------------------------------------------------
+# Button ---------------------------------------------------------------------
 var=Button(master=fenster, text = '', command=fkt)
 #mit () dahinter wird fkt sofort ausgeführt
 
-#Frame ------------------------------------------------------------------------------------
+# Frame ----------------------------------------------------------------------
 var=Frame(master=fenster)
 #Container (für Layout und Anordnung praktisch) (statt Fenster frame als master)
 
-#Entry ------------------------------------------------------------------------------------
+# Entry ----------------------------------------------------------------------
 var=Entry(master=fenster)
 #einzeilige Eingabe; Text für mehrzeilig
 var.get()							#Rückgabe (Auch Indexe wie Delete möglich)
-var.delete(index[, index2])		#löscht Zeichen, oder von 1 bis 2 
-var.insert(index, text) 		#fügt an index text ein
+var.delete(index[, index2])			#löscht Zeichen, oder von 1 bis 2 
+var.insert(index, text) 			#fügt an index text ein
 
-#Radiobuttons -----------------------------------------------------------------------------
+# Radiobuttons ---------------------------------------------------------------
 varKontroll=StringVar()
 varButton1=Radiobutton(master=fenster, text='', value=varKontroll, variable=Wertvariable)
 varButton2=Radiobutton(master=fenster, text='', value=varKontroll, variable=Wertvariable2)
-varAusgabe=varKontroll.get()
-#1 aus n Auswahl; Kontrollvariable für alle nötig, nimmt Wert für ausgewählten an (deswegen get zum Wert bekommen!)
-#Checkbutton
+varAusgabe=varKontroll.get() 
+#Kontrollvariable für alle nötig, nimmt Wert für ausgewählten an (deswegen get zum Wert bekommen!)
+varButton1.select()		#Vorauswahl
+#1 aus n Auswahl
+
+# Checkbutton ----------------------------------------------------------------
 #m aus n Auswahl
 
-#Bilder -----------------------------------------------------------------------------------
-photoimage=PhotoImage(master=fenster, file=pfad)
-#Bild, nur png!
+# Bilder ---------------------------------------------------------------------
+photoimage=PhotoImage(master=fenster, file=pfad)		#Bild, nur png!
 photoimage.get(x,y)		#Gibt Farbwert als Tupel des Pixels zurück
 photoimage.height()		#gibt Höhe Foto zurück /width
 photoimage.put(farbe,position)
@@ -211,14 +226,14 @@ photoimage.write(pfad)	#speichert Bild in Pfad
 #für andere Dateien:
 from PIL import *
 varPIL=Image.open(pfad)
-varTK=ImageTk.PhotoImage(varPIL)	#notwendig für Darstellung
+varTK=ImageTK.PhotoImage(varPIL)	#notwendig für Darstellung
 
 varPIL.getpixel((x,y))				#entspricht get
 varPIL.putpixel((x,y),(r,g,b))		#entspricht put
 varPIL.thumbnail(size=(w,h))		#verkleinert auf width und height
 varPIL.size()						#gibt Größe in w und h zurück
 
-#Widgetmethoden ---------------------------------------------------------------------------
+# Widgetmethoden -------------------------------------------------------------
 Widget.pack()		 		#erzeugt Widget
 Widget.destroy()			#löscht W
 Widget.cget(text)			#gibt Zustand des W wieder, Bsp: text
@@ -226,26 +241,26 @@ Widget.config(text='b')		#ändert Wert, Bsp: text='b'
 Widget.bell()				#erzeugt Glockenschlag
 Widget.after(zeit, fkt)		#nach Millisekunden wird Fkt. ausgeführt
 
-#Widget anordnen --------------------------------------------------------------------------
-Für pack():
-anchor		#CENTRE, E, N, NE, NW, S, SE, SW, W
-expand		#1-->ändert Größe mit Großziehen von fenster (0 nicht)
-padx/pady	#leerer Raum drumherum
-side		#LEFT,RIGHT,TOP,BUTTOM ; an dessen Rand gesetzt
-fill		#passt sich an Master an (Widget füllt mit leeren Raum): X,Y,BOTH,NONE
-
 #Widgetattribute (für die meisten)
 text
 image			#Bild auf Widget zu sehen	
 height
 width
 font=(Schriftart, Schriftgröße)
-justify		#Ausrichtung Text des Widgets		
-fg/bg			#Hintergrund/Vordergrundfarbe, mit String oder 				#Hexadezimal
+justify			#Ausrichtung Text des Widgets		
+fg/bg			#Hintergrund/Vordergrundfarbe, mit String oder Hexadezimal
 bd/borderwith
-relief		#Form Rahmen; SUNKEN,RAISED,GROOVE,RIDGE,FLAT
+relief			#Form Rahmen; SUNKEN,RAISED,GROOVE,RIDGE,FLAT
 
-#Menübar ----------------------------------------------------------------------------------
+# Widget anordnen ------------------------------------------------------------
+Für pack():
+anchor			#CENTRE, E, N, NE, NW, S, SE, SW, W
+expand			#1-->ändert Größe mit Großziehen von fenster (0 nicht)
+padx/pady		#leerer Raum drumherum
+side			#LEFT,RIGHT,TOP,BUTTOM ; an dessen Rand gesetzt
+fill			#passt sich an Master an (Widget füllt mit leeren Raum): X,Y,BOTH,NONE
+
+# Menübar --------------------------------------------------------------------
 varMenu=Menu(master=fenster)
 fenster.config(menu=varMenu)	#Fenster hat Menü
 varerstes=Menu(varMenu)
@@ -256,15 +271,15 @@ varerstes.add_command(label="Hi!", command=fkt)
 varerstes.add_separator() 		#Trennstrich
 #Man muss nicht pack()
 
-# Threads ---------------------------------------------------------------------------------
+# Threads --------------------------------------------------------------------
 from _thread import start_new_thread
 start_new_thread(fkt, (...))	#erst Fkt., dann übergebene Var, für leer: ()
 #neuer Ablauf des Programms --> mehrere Abläufe parallel (Widgets #können sich möglicherweise nicht aktualisieren, dann mit neuen Thread probieren)
 
-# Assert ----------------------------------------------------------------------------------
+# Assert ---------------------------------------------------------------------
 assert [Bedingung]		#bricht Programm ab, wenn falsch
 
-# Objektorientierung ----------------------------------------------------------------------
+# Objektorientierung ---------------------------------------------------------
 class klassenname:
 	#[Docstring (Erklärung als str)]
 	def __init__(self, weiteresAttribut):
@@ -277,7 +292,7 @@ class klassenname:
 a=klassenname()
 a.Methode1()
 
-# Turtle ----------------------------------------------------------------------------------
+# Turtle ---------------------------------------------------------------------
 import turtle
 var = turtle.Turtle()
 var.pendown()				
